2b255b0121955c4382d13c4cca6e6c02883273ef
Yoel Diomedez <yoeldiomedez@gmail.com>
2021-04-24 17:25:13 -0500

    searchbar added

--- src/app/userslist/userslist.page.ts
+++ src/app/userslist/userslist.page.ts
@@ -10,40 +10,51 @@ import { HttpClient } from '@angular/common/http';
 export class UserslistPage implements OnInit {
   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
+  toggledSearchBar: Boolean;
   characters: Array<any>;
-  pages: Number;
-
-  page = 1;
-  status = '';
-  gender = '';
-
-  endPoint = (page, status='', gender='') => `https://rickandmortyapi.com/api/character?page=${page}&status=${status}&gender=${gender}`;
+  nextPage: string;
   
-  constructor(
-    private http: HttpClient
-  ) { }
+  name: string;
+  status: string;
+  gender: string;
+  
+  constructor(private http: HttpClient) { 
+    this.toggledSearchBar = false;
+    this.name = '';
+    this.status = '';
+    this.gender = '';
+  }
 
   ngOnInit() {
-    this.http.get<any>(this.endPoint(this.page)).subscribe(
+    this.http.get<any>('https://rickandmortyapi.com/api/character').subscribe(
       res => {
         // console.log(res);
         this.characters = res.results;
-        this.pages = res.info.pages;
+        this.nextPage = res.info.next;
       }
     )
   }
 
-  loadMoreCharacters(event) {
-    this.page++;
-    if (this.page == this.pages) {
+  getEndpoint(name:string, status:string, gender:string) {
+    return `https://rickandmortyapi.com/api/character?name=${name}&status=${status}&gender=${gender}`;
+  }
+  
+  toggleSearchBar() {
+    this.toggledSearchBar = !this.toggledSearchBar;
+  }
+
+  loadMoreCharacters(event:any) {
+    if (this.nextPage == null) {
       event.target.disabled = true;
     } else {
-      this.http.get<any>(this.endPoint(this.page, this.status, this.gender)).subscribe(
+      this.http.get<any>(this.nextPage).subscribe(
         res => {
           // console.log(res);
+          this.nextPage = res.info.next;
           for (const character of res.results) {
             this.characters.push(character)
           }
+          // console.log(this.nextPage)
           event.target.complete();
         }
       )
@@ -52,28 +63,42 @@ export class UserslistPage implements OnInit {
 
   getByStatusCharacter(statusCharacter){
     this.status = statusCharacter;
-    this.http.get<any>(this.endPoint(this.page, this.status, this.gender)).subscribe(
+    this.http.get<any>(this.getEndpoint(this.name, this.status, this.gender)).subscribe(
       res => {
         // console.log(res);
         while(this.characters.length > 0) {
           this.characters.pop();
         }
         this.characters = res.results;
-        this.pages = res.info.pages;
+        this.nextPage = res.info.next;
       }
     )
   }
 
   getByGenderCharacter(genderCharacter){
     this.gender = genderCharacter;
-    this.http.get<any>(this.endPoint(this.page, this.status, this.gender)).subscribe(
+    this.http.get<any>(this.getEndpoint(this.name, this.status, this.gender)).subscribe(
       res => {
         // console.log(res);
         while(this.characters.length > 0) {
           this.characters.pop();
         }
         this.characters = res.results;
-        this.pages = res.info.pages;
+        this.nextPage = res.info.next;        
+      }
+    )
+  }
+
+  searchByCharacterName(event:any){
+    this.name = event.target.value.trim();
+    this.http.get<any>(this.getEndpoint(this.name, this.status, this.gender)).subscribe(
+      res => {
+        // console.log(res);
+        while(this.characters.length > 0) {
+          this.characters.pop();
+        }
+        this.characters = res.results;
+        this.nextPage = res.info.next;
       }
     )
   }
